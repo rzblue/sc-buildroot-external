bf7cdd614610b95ea4fa0036b317921e9a535c4d
Ryan Blue <ryanzblue@gmail.com>
2025-06-14 02:12:01 -0400

    run fsck on /boot

--- board/systemcoreathome/post-build/00-boot-fstab.sh
+++ board/systemcoreathome/post-build/00-boot-fstab.sh
@@ -2,7 +2,7 @@
 
 # Add boot partition to fstab at /boot
 
-BOOT_FSTAB="/dev/mmcblk0p1 /boot vfat defaults 0 0"
+BOOT_FSTAB="/dev/mmcblk0p1 /boot vfat defaults 0 2"
 if ! grep -Fsq /dev/mmcblk0p1 "${TARGET_DIR}"/etc/fstab; then
   echo "${BOOT_FSTAB}" >> "${TARGET_DIR}"/etc/fstab
 fi
